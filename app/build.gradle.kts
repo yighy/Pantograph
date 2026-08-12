3ea4627ba3cda595b0f0595f71ed96b38cf6b79b
yighy <yighy@users.noreply.github.com>
2026-08-12 23:50:39 +0300

    Update interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 31
         targetSdk = 37
         versionCode = 1
-        versionName = "0.2.3"
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
