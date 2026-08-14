aaf76cd03201c5b1a6452f02bbd9ef09e577d307
yighy <yighy@users.noreply.github.com>
2026-08-15 01:26:03 +0300

    Bump version to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 31
         targetSdk = 37
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
