097f1b33801492178581963fea9de5abd8d98e4b
yighy <yighy@users.noreply.github.com>
2026-08-17 01:11:01 +0300

    Bump version to 0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 31
         targetSdk = 37
         versionCode = 1
-        versionName = "0.6.1"
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
