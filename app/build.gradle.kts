e8c9ea6b51cd03c6126530b68af20e58fd4f1171
yighy <yighy@users.noreply.github.com>
2026-08-26 21:53:51 +0300

    Bump version to 0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.yighy.pantograph"
         minSdk = 31
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.9.1"
+        versionCode = 2
+        versionName = "0.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
